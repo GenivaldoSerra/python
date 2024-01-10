3eca1adbcec5bae2dd51081ed62a0e4882858699
Genivaldo <genivaldo.serra@live.com>
2024-01-10 16:20:00 -0300

    feat: Inserindo os comandos para login no sistema.

--- htg_python/aula_01/codigo.py
+++ htg_python/aula_01/codigo.py
@@ -1,5 +1,5 @@
-import pyautogui as pag
-import pyautogui._pyautogui_win as platformModule
+import pyautogui
+import time
 
 # Prcipais comandos do PyAutoGUI
 # pyautogui.position() -> retorna a posição do mouse
@@ -11,4 +11,38 @@ import pyautogui._pyautogui_win as platformModule
 # pyautogui.write('texto') -> escreve o texto
 # pyautogui.press('enter') -> pressiona a tecla enter
 
-platformModule.press('capslock')
+link = 'https://dlp.hashtagtreinamentos.com/python/intensivao/login'
+
+pyautogui.PAUSE = 1
+
+# Acessar o navegador Edge
+pyautogui.press("win")
+
+pyautogui.write("edge")
+
+pyautogui.press("enter")
+
+# Digitar o link
+pyautogui.write(link)
+
+# Pressionar a tecla enter
+pyautogui.press("enter")
+
+# Aguardar 5 segundos
+# pyautogui.sleep(5)
+time.sleep(10)
+
+# Clicar no campo de e-mail
+pyautogui.click(x=-770, y=300)
+
+# Digitar o e-mail
+pyautogui.write('teste@teste.com')
+
+# Clicar no campo de senha
+pyautogui.click(x=-835, y=374)
+
+# Digitar a senha
+pyautogui.write('teste')
+
+# Clicar no botão entrar
+pyautogui.click(x=-651, y=434)
