4e28c506b6b2c277dfe24b47eec120cb44a32673
Genivaldo <genivaldo.serra@live.com>
2024-01-10 11:03:07 -0300

    feat: Incluindo o arq código.

---
+++ htg_python/aula_01/codigo.py
@@ -0,0 +1,14 @@
+import pyautogui as pag
+import pyautogui._pyautogui_win as platformModule
+
+# Prcipais comandos do PyAutoGUI
+# pyautogui.position() -> retorna a posição do mouse
+# pyautogui.moveTo(x, y, duration) -> move o mouse para a posição x, y em um tempo duration
+# pyautogui.click(x, y) -> clica na posição x, y
+# pyautogui.doubleClick(x, y) -> clica duas vezes na posição x, y
+# pyautogui.rightClick(x, y) -> clica com o botão direito na posição x, y
+# pyautogui.dragTo(x, y, duration) -> arrasta o mouse para a posição x, y em um tempo duration
+# pyautogui.write('texto') -> escreve o texto
+# pyautogui.press('enter') -> pressiona a tecla enter
+
+platformModule.press('capslock')
