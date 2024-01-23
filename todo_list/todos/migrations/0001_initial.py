39e9f13b24169db27dba7a9ae2dc03e7c66ad6be
Genivaldo <genivaldo.serra@live.com>
2024-01-23 17:35:56 -0300

    feat: Configurando o linter

--- todo_list/todos/migrations/0001_initial.py
+++ todo_list/todos/migrations/0001_initial.py
@@ -4,22 +4,28 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Todo',
+            name="Todo",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=100)),
-                ('description', models.TextField(max_length=500)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('deadline', models.DateField()),
-                ('finished_at', models.DateField(blank=True, null=True)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=100)),
+                ("description", models.TextField(max_length=500)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("deadline", models.DateField()),
+                ("finished_at", models.DateField(blank=True, null=True)),
             ],
         ),
     ]
--- todo_list/todos/models.py
+++ todo_list/todos/models.py
@@ -3,6 +3,7 @@ from datetime import date
 
 # Create your models here.
 
+
 class Todo(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
     description = models.TextField(max_length=500, null=False, blank=False)
