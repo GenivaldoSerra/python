7896aa706b11d951be2539fb1e641a3f7d0a06f2
Genivaldo <genivaldo.serra@live.com>
2024-01-23 17:25:07 -0300

    feat: Criando o banco de dados.

--- todo_list/setup/settings.py
+++ todo_list/setup/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config, Csv
+from dj_database_url import parse as dburl
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hr__q&&04!mxh8t93i9r^j!(llh-x#v#-o)i4h!4#^s#1cqft%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -75,10 +78,8 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': config('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}', cast=dburl),
+    # 'default': config('DATABASE_URL', default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'), cast=dburl),
 }
 
 
--- todo_list/todos/models.py
+++ todo_list/todos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(max_length=500, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField(null=False, blank=False)
+    finished_at = models.DateField(null=True, blank=True)
